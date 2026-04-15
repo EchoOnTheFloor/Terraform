180ffcfd3a1e50f241af92b0c9fa2caac0442ce4
unknown <you@example.com>
2026-04-15 15:24:22 +0200

    M (network README database)

---
+++ network.tf
@@ -0,0 +1,16 @@
+# VPC
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-network"
+}
+# SUBNET
+
+
+# Router
+
+# NAT
+
+# Firewall SSH
+
+# Firewall HTTP
+
+# Global private address
